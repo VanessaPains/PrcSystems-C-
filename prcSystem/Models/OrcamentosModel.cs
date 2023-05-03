eb1ebc42e62d3d7bd8924194fbc3a7431aefd44e
Vanessa Pains Fonseca <35502727+VanessaPains@users.noreply.github.com>
2023-05-03 17:26:16 -0300

    Alterações tele de orçamentos: - inicio implementação Models.

---
+++ prcSystem/Models/OrcamentosModel.cs
@@ -0,0 +1,23 @@
+﻿using prcSystem.Controllers;
+using prcSystem.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prcSystem.Models
+{
+    public class OrcamentosModel
+    {
+        /// <summary>
+        /// lista os clientes e fornecedores do PessoaController
+        /// </summary>
+        /// <returns>lista dos clientes e fornecedores na tela de lançamentos de entradas e saidas</returns>
+        public List<Pessoa> ListarCliForn()
+        {
+            return new PessoaController().Listar();
+        }
+
+    }
+}
